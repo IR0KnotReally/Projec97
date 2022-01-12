6ea2b29601b762c4bfabc4503f06598b35f94473
IR0KnotReally <71969213+IR0KnotReally@users.noreply.github.com>
2022-01-12 17:41:56 +0530

    Update pro97.py

--- pro97.py
+++ pro97.py
@@ -6,11 +6,11 @@ print("Guess a number between 1 - 9")
 while tries<3:
     guess = int(input("Enter your guess: "))
     if guess == num:
-        print("You guessed the number!")
+        print("You guessed the number!!")
     elif(guess<num):
         print("You guessed too low")
     else:
         print("You guessed too high")
         tries=tries+1
 if tries==3:
-    print("You lost, try again. The number was: ", num)
+    print("You lost, Try again. The real number was: ", num)
